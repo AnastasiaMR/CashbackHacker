97689e9901c720115e2305ff9e4c8d87fb203bbd
Anastasia <matr-nastya@yandex.ru>
2021-05-24 23:16:19 +0300

    Add branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,37 +1,37 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
     public CashbackHackService cashbackHacker = new CashbackHackService();
 
     @Test
-    void shouldCashBackSum1100( ) {
+    public void shouldCashBackSum1100( ) {
         int actual = cashbackHacker.remain(1100);
         int expected = 900;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCashBackSum500( ) {
+    public void shouldCashBackSum500( ) {
         int actual = cashbackHacker.remain(500);
         int expected = 500;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCashBackSum1000( ) {
+    public void shouldCashBackSum1000( ) {
         int actual = cashbackHacker.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCashBackSum3900( ) {
+    public void shouldCashBackSum3900( ) {
         int actual = cashbackHacker.remain(3900);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
