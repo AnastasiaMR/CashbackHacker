a6595ef6dbe7392689cb39398df3fb4268214bc4
Anastasia <matr-nastya@yandex.ru>
2021-05-24 19:19:04 +0300

    Add TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 class CashbackHackServiceTest {
     public CashbackHackService cashbackHacker = new CashbackHackService();
@@ -11,27 +11,27 @@ class CashbackHackServiceTest {
     void shouldCashBackSum1100( ) {
         int actual = cashbackHacker.remain(1100);
         int expected = 900;
-        assertEquals(expected,actual);
+        assertEquals(actual, expected);
     }
 
     @Test
     void shouldCashBackSum500( ) {
         int actual = cashbackHacker.remain(500);
         int expected = 500;
-        assertEquals(expected,actual);
+        assertEquals(actual, expected);
     }
 
     @Test
     void shouldCashBackSum1000( ) {
         int actual = cashbackHacker.remain(1000);
         int expected = 0;
-        assertEquals(expected,actual);
+        assertEquals(actual, expected);
     }
 
     @Test
     void shouldCashBackSum3900( ) {
         int actual = cashbackHacker.remain(3900);
         int expected = 100;
-        assertEquals(expected,actual);
+        assertEquals(actual, expected);
     }
 }
